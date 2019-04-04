2378de71ca283be0a4599bba7ae7329e4cf6db7a
TanakaHideakiGitHub <tanakahide0703@gmail.com>
2019-04-04 22:52:13 +0900

    プラグインやUtil系スクリプト導入。

---
+++ Assets/Editor/AdjustUGUIAnchorExtention.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+/// <summary>
+/// UGUIのアンカーをオブジェクトサイズに合わせる。
+/// ただし、localScaleの値がxy共に1のときのみ
+/// </summary>
+public static class AdjustUGUIAnchorExtention
+{
+	[MenuItem("Util/AdjustAnchor")]
+	static void FitAnchorsToTransform()
+	{
+		var rectTransforms = from gameObject in Selection.gameObjects
+		                     where gameObject.GetComponent<RectTransform>() != null
+		                     select gameObject.GetComponent<RectTransform>();
+
+		if (rectTransforms == null || rectTransforms.Count() <= 0)
+		{
+			Debug.Log("No RectTransform is selected.");
+			return;
+		}
+
+		Undo.RecordObjects(rectTransforms.ToArray(), "Fit Anchors");
+
+		foreach (var item in rectTransforms)
+			FitAnchorsToTransform(item);
+	}
+
+	static void FitAnchorsToTransform(RectTransform rectTransform)
+	{
+		var parent = rectTransform.parent as RectTransform;
+		if (parent == null)
+            return;
+
+        if(Mathf.RoundToInt(rectTransform.localScale.x * 1000f) != 1000 || Mathf.RoundToInt(rectTransform.localScale.y * 1000f) != 1000)
+        {
+            Debug.LogError("UGUIオブジェクトのlocalScaleが1じゃない");
+            return;
+        }
+
+        var canvasWidth = parent.rect.width;
+        var canvasHeight = parent.rect.height;
+
+        var x = rectTransform.localPosition.x + canvasWidth * 0.5f;
+        var y = rectTransform.localPosition.y + canvasHeight * 0.5f;
+        var w = rectTransform.rect.width;
+        var h = rectTransform.rect.height;
+        
+        var lx = x - w / 2;
+        var ly = y + h / 2;
+        var rx = x + w / 2;
+        var ry = y - h / 2;
+        var min = new Vector2(lx / canvasWidth, ly / canvasHeight);
+        var max = new Vector2(rx / canvasWidth, ry / canvasHeight);
+        
+        rectTransform.anchorMin = new Vector2(min.x, max.y);
+        rectTransform.anchorMax = new Vector2(max.x, min.y);
+
+        // アンカーからの距離はゼロに設定
+        rectTransform.offsetMin = Vector2.zero;
+        rectTransform.offsetMax = Vector2.zero;
+    }
+}
---
+++ Assets/Scripts/Utils/Utils.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Tanaka
+{
+    public static class Utils
+    {
+
+        /// <summary>
+        /// 保存パス取得
+        /// </summary>
+        /// <param name="tex"></param>
+        /// <returns></returns>
+        public static string GetWriteFolderPath(string texName)
+        {
+#if !UNITY_EDITOR
+            return UtilsAndroid.GetExternalStorageFileDirectory() + "/ScreenShots/" + texName + "/";
+#else
+            return Application.streamingAssetsPath + "/" + texName + "/";
+#endif
+        }
+
+        /// <summary>
+        /// IOを利用した直接的なファイルロード(Androidでは不可)
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public static Texture2D LoadTextureByFileIO(string path, int width, int height)
+        {
+            var tex = new Texture2D(width, height);
+            byte[] b = File.ReadAllBytes(path);
+            tex.LoadImage(b, false);
+            return tex;
+        }
+        /// <summary>
+        /// UnityWebRequestを利用したファイルロード
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public static Texture2D LoadTextureByWebRequest(string path, int width, int height)
+        {
+            var tex = new Texture2D(width, height);
+            var enumerator = LoadTextureByWebRequest(path, 100, 100, tex);
+            while (enumerator.MoveNext()) { /*Debug.Log(Time.realtimeSinceStartup);*/ }
+            return tex;
+        }
+        private static IEnumerator LoadTextureByWebRequest(string path, int width, int height, Texture2D texture)
+        {
+            var request = UnityWebRequest.Get(path);
+            yield return request.SendWebRequest();
+
+            while(!request.isDone)
+                yield return null;
+
+            byte[] b = request.downloadHandler.data;
+            texture.LoadImage(b, false);
+        }
+
+        /// <summary>
+        /// Ease-In。最初ゆっくり、後半速い。
+        /// </summary>
+        /// <param name="start">開始値</param>
+        /// <param name="end">終了値</param>
+        /// <param name="elapsed">Ease経過時間</param>
+        /// <param name="time">Easeさせる時間</param>
+        /// <returns></returns> 
+        public static float EaseIn(float start, float end, float elapsed, float time)
+        {
+            elapsed /= time;
+            var dif = end - start;
+            return dif * elapsed * elapsed + start;
+        }
+        public static Vector2 EaseIn(Vector2 start, Vector2 end, float elapsed, float time)
+        {
+            elapsed /= time;
+            var dif = end - start;
+            return dif * elapsed * elapsed + start;
+        }
+
+        /// <summary>
+        /// Ease-Out。最初速い、後半ゆっくり。
+        /// </summary>
+        /// <param name="start">開始値</param>
+        /// <param name="end">終了値</param>
+        /// <param name="elapsed">Ease経過時間</param>
+        /// <param name="time">Easeさせる時間</param>
+        /// <returns></returns> 
+        public static float EaseOut(float start, float end, float elapsed, float time)
+        {
+            elapsed /= time;
+            var dif = end - start;
+            return -dif * Mathf.Pow(elapsed - 1f, 2) + end;
+        }
+        public static Vector2 EaseOut(Vector2 start, Vector2 end, float elapsed, float time)
+        {
+            elapsed /= time;
+            var dif = end - start;
+            return -dif * Mathf.Pow(elapsed - 1f, 2) + end;
+        }
+
+        /// <summary>
+        /// Ease-InOut。最初ゆっくり、最後もゆっくり。
+        /// </summary>
+        /// <param name="start">開始値</param>
+        /// <param name="end">終了値</param>
+        /// <param name="elapsed">Ease経過時間</param>
+        /// <param name="time">Easeさせる時間</param>
+        /// <returns></returns> 
+        public static float EaseInOut(float start, float end, float elapsed, float time)
+        {
+            var result = 0f;
+            var half = time * 0.5f;
+            var dif = end - start;
+            if (elapsed / time < 0.5f)
+                result = EaseIn(start, start + dif * 0.5f, elapsed, half);
+            else
+                result = EaseOut(start + dif * 0.5f, end, elapsed - half, half);
+            return result;
+        }
+        public static Vector2 EaseInOut(Vector2 start, Vector2 end, float elapsed, float time)
+        {
+            var result = Vector2.zero;
+            var half = time * 0.5f;
+            var dif = end - start;
+            if (elapsed / time < 0.5f)
+                result = EaseIn(start, start + dif * 0.5f, elapsed, half);
+            else
+                result = EaseOut(start + dif * 0.5f, end, elapsed - half, half);
+            return result;
+        }
+    }
+}
